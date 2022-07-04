0cc0a6fad32959e8ce4e77279505f88ac3da6d0c
fivaldivia <fivaldivia@uc.cl>
2022-07-04 18:44:05 -0400

    feat(second exercise):second exercise ready but takes too long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from functions.count_letters import count_letters_in_name
+import requests
+
 
 def main():
     results = []
@@ -22,6 +24,34 @@ def main():
     char_counter_json = { "exercise_name": "Char counter","time": execution_time,
         "in_time": in_time,"results":results_char_counter}
     results.append(char_counter_json)
-        
+
+    ### Second exercise ###
+    # Start counting execution time
+    start_time = time.time()
+    ##
+    response = requests.get('https://rickandmortyapi.com/api/episode').json()
+    total_pages = response['info']['pages']
+    results_episode_locations = []
+    for page in range(1,total_pages+1):
+        page_url = 'https://rickandmortyapi.com/api/episode'+'/?page='+str(page)
+        results_list = requests.get(page_url).json()['results']
+        for item in results_list:
+            locations = []
+            for character_url in item['characters']:
+                character_location = requests.get(character_url).json()['location']['name']
+                if character_location not in locations:
+                    locations.append(character_location)
+            episode_result = {"name": item['name'],
+                    "episode": item['episode'],
+                    "locations": locations}
+            results_episode_locations.append(episode_result)
+    # Get execution time
+    execution_time = time.time() - start_time
+    # Generate json and add to results
+    in_time = True if execution_time<=3 else False
+    episode_locations_json = { "exercise_name": "Episode locations","time": execution_time,
+        "in_time": in_time,"results":results_episode_locations}
+    results.append(episode_locations_json) 
+       
 if __name__ == "__main__":
     main()
